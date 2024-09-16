e49a66e1f3f4bf086f51a1ccaba08b4344c1c336
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-09-15 21:46:50 -0300

    :sparkles: feat: relevant posts added to homepage

---
+++ src/components/RelevantPosts/index.jsx
@@ -0,0 +1,88 @@
+import "./styles.css"
+import redMarker from "../../assets/red-marker.svg"
+import relevantButton from "../../assets/like-icon.svg"
+import { Link } from "react-router-dom"
+
+export default function RelevantPosts() {
+  return (
+    <>
+      <div id="relevant_posts">
+        <h2>Postagens mais relevantes</h2>
+
+        <div id="posts">
+          <div className="post">
+            <img src="https://beartac-imgs.s3.sa-east-1.amazonaws.com/uploads/DSC08361-1300x731.jpg" alt="post-image" />
+            <div className="post-content">
+              <div className="post-header">
+                <div className="locale">
+                  <img src={redMarker} alt="red-marker" />
+                  <p>Rua Ernesto Nicoline 109</p>
+                </div>
+                <p className="date">23/09/2024</p>
+              </div>
+              <div className="post-body">
+                <p>
+                  <strong>José de Camargo:</strong> Essas guerras de arminhas de gel estão preocupando moradores da região. Muitas crianças estão brincando desse negócio está metendo o apavoro em todo mundo!
+                </p>
+              </div>
+              <div className="post-footer">
+                <button className="relevant-doubts-btn">
+                  <img src={relevantButton} alt="like" /> <span>Relevante</span>
+                </button>
+                <Link to={""}>Ver denuncia</Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="post">
+            <img src="https://assets.b9.com.br/wp-content/uploads/2018/04/71749-1200x720.jpg" alt="post-image" />
+            <div className="post-content">
+              <div className="post-header">
+                <div className="locale">
+                  <img src={redMarker} alt="red-marker" />
+                  <p>Rua do Diadema, 493, Centro de Diadema</p>
+                </div>
+                <p className="date">11/09/2024</p>
+              </div>
+              <div className="post-body">
+                <p>
+                  <strong>Zézé de Luciano:</strong> Cheiro absurdo de maconha na região. Essa gente não tem respeito pelas pessoas, ficam F1 em qualquer lugar, e a gente tem que aguentar esse cheiro maldito!
+                </p>
+              </div>
+              <div className="post-footer">
+                <button className="relevant-doubts-btn">
+                  <img src={relevantButton} alt="like" /> <span>Relevante</span>
+                </button>
+                <Link to={""}>Ver denuncia</Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="post">
+            <img src="https://ogimg.infoglobo.com.br/in/4065262-96e-0ed/FT1086A/Gringos-dancam-ate-o-chao-no-baile-funk-do-Emocoes-na-RocinhaFoto-deEduardo-Naddar.jpg" alt="post-image" />
+            <div className="post-content">
+              <div className="post-header">
+                <div className="locale">
+                  <img src={redMarker} alt="red-marker" />
+                  <p>Rua Erlindo de Emanuela 124</p>
+                </div>
+                <p className="date">22/09/2024</p>
+              </div>
+              <div className="post-body">
+                <p>
+                  <strong>Viviana Santana:</strong> Música muito alta! Baile funk rolando solto aqui na comunidade desde às 23:00. Eu querendo durmir, tenho que trampar amanhã e essa gentalha na curtição.
+                </p>
+              </div>
+              <div className="post-footer">
+                <button className="relevant-doubts-btn">
+                  <img src={relevantButton} alt="like" /> <span>Relevante</span>
+                </button>
+                <Link to={""}>Ver denuncia</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/templates/Homepage/index.jsx
+++ src/templates/Homepage/index.jsx
@@ -1,13 +1,17 @@
-import Header_homepage from "../../components/Header_homepage";
-import Footer_homepage from "../../components/Footer_homepage";
-import HowCanHelp from "../../components/HowCanHelp";
-import RecentComplaints from "../../components/RecentComplaints";
+import Header_homepage from "../../components/Header_homepage"
+import Footer_homepage from "../../components/Footer_homepage"
+import HowCanHelp from "../../components/HowCanHelp"
+import RecentComplaints from "../../components/RecentComplaints"
+import RelevantPosts from "../../components/RelevantPosts"
 
 export default function Homepage() {
-  return <>
-    <Header_homepage/>
-    <HowCanHelp />
-    <RecentComplaints/>
-    <Footer_homepage/>
-  </>
+  return (
+    <>
+      <Header_homepage />
+      <HowCanHelp />
+      <RecentComplaints />
+      <RelevantPosts />
+      <Footer_homepage />
+    </>
+  )
 }
