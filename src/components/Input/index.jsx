8ce1250304f7bd56f3c087dd5bbdd2785d769156
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-09-29 13:42:22 -0300

    :sparkles: feat: system to see the input password

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import eyeClose from "../../assets/eye-close-icon.svg"
+import eyeOpen from "../../assets/eye-open-icon.svg"
 import "./styles.css"
 
 export default function Input({
@@ -8,25 +11,57 @@ export default function Input({
   onChangeInput,
   maximum,
   minimum,
-  maximumOfLetters
+  maximumOfLetters,
 }) {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleSeePasswordButtonClick = () => {
+    setShowPassword(!showPassword)
+  }
+
   return (
     <>
-      <label className="label-input" htmlFor={idName}>
-        {label}
-        <input
-          type={type}
-          name={idName}
-          id={idName}
-          className="input"
-          placeholder={placeholder}
-          onChange={onChangeInput}
-          max={maximum}
-          min={minimum}
-          maxLength={maximumOfLetters}
-          required
-        />
-      </label>
+      {type === "password" ? (
+        <label className="label_input" htmlFor={idName}>
+          {label}
+          <div id="password_container">
+            <input
+              type={!showPassword ? "password" : "text"}
+              name={idName}
+              id={idName}
+              placeholder={placeholder}
+              onChange={onChangeInput}
+              max={maximum}
+              min={minimum}
+              maxLength={maximumOfLetters}
+              required
+            />
+            <button type="button" onClick={handleSeePasswordButtonClick}>
+              {showPassword ? (
+                <img src={eyeOpen} className="eye" alt="eye-open" />
+              ) : (
+                <img src={eyeClose} className="eye" alt="eye-close" />
+              )}
+            </button>
+          </div>
+        </label>
+      ) : (
+        <label className="label_input" htmlFor={idName}>
+          {label}
+          <input
+            type={type}
+            name={idName}
+            id={idName}
+            className="input"
+            placeholder={placeholder}
+            onChange={onChangeInput}
+            max={maximum}
+            min={minimum}
+            maxLength={maximumOfLetters}
+            required
+          />
+        </label>
+      )}
     </>
   )
 }
