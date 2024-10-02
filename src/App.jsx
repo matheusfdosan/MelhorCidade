072b3d45fd4b98e2f25badd529a90f6d50bdb302
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-10-01 23:31:48 -0300

    :construction: starting the creation of the report page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Landing from "./templates/Landing"
 import Login from "./templates/Login"
 import SignUp from "./templates/SignUp"
 import Homepage from "./templates/Homepage"
+import Report from "./templates/Report"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/report" element={<Report />} />
       </Routes>
     </Router>
   )
