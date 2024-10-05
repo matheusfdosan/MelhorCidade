8113e5eeae51ccfc60bd1e939b538069e85327bc
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-10-05 08:50:49 -0300

    :sparkles: feat: settings screen added

---
+++ src/templates/Settings/index.jsx
@@ -0,0 +1,64 @@
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import rightArrow from "../../assets/right-arrow-icon.svg"
+import "./styles.css"
+import { useState } from "react"
+
+export default function Settings() {
+  const [darkTheme, setDarkTheme] = useState(false)
+
+  const handleClickToggler = () => {
+    setDarkTheme(!darkTheme)
+  }
+
+  return (
+    <>
+      <Header />
+      <main id="settings_container">
+        <section id="your_account">
+          <h2>Sua Conta</h2>
+          <div class="settings">
+            <h3>Gregory Singleton</h3>
+            <p>gregory.singleton@email.com</p>
+          </div>
+        </section>
+        <section id="visibility">
+          <h2>Visibilidade</h2>
+          <div class="settings">
+            <p>Tema Escuro</p>
+            <div
+              className={`wrapper ${darkTheme}`}
+              onClick={handleClickToggler}
+            >
+              <div id="toggler"></div>
+            </div>
+          </div>
+        </section>
+        <section id="manage_account">
+          <h2>Gerenciar Conta</h2>
+          <ul class="settings">
+            <li>
+              <p>Mudar senha</p>
+              <img src={rightArrow} alt="right-arrow" />
+            </li>
+            <li>
+              <p>Endereço</p>
+              <span>Ghana, Willie Morgan, 62</span>
+              <img src={rightArrow} alt="right-arrow" />
+            </li>
+            <li>
+              <p>Telefone</p>
+              <span>+55 91234-5678</span>
+              <img src={rightArrow} alt="right-arrow" />
+            </li>
+          </ul>
+        </section>
+        <section id="delete_account">
+          <p>Deletar Conta</p>
+          <button>Deletar</button>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
