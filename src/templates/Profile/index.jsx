bdca4244a63ffc302427c55273e01b8844e813d9
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-10-06 13:14:42 -0300

    :sparkles: feat: user profile screen made

---
+++ src/templates/Profile/index.jsx
@@ -0,0 +1,46 @@
+import "./styles.css"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import Posts from "../../components/Posts"
+
+export default function Profile() {
+  return (
+    <>
+      <Header />
+      <main id="profile_container">
+        <div id="user_info">
+          <div id="profile_picture_container">
+            <img
+              src="https://www.designi.com.br/images/preview/12161378.jpg"
+              alt="user_picture"
+            />
+          </div>
+
+          <div id="user_datas">
+            <h3>Gregory Singleton</h3>
+            <ul>
+              <li>
+                <span className="amout">13</span>
+                <span className="of_something">Seguidores</span>
+              </li>
+              <li>
+                <span className="amout">43</span>
+                <span className="of_something">Seguindo</span>
+              </li>
+              <li>
+                <span className="amout">3</span>
+                <span className="of_something">Postagens</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <h2>Suas Postagens</h2>
+        <div id="posts_container">
+          <Posts />
+        </div>
+      </main>
+      <Footer target={2} />
+    </>
+  )
+}
