f0b0db40788c1c827852799d9e94454790fb0597
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-10-31 07:33:21 -0300

    :wrench: chore: header for a request, informing the server that the request body contains data in JSON format

--- src/utils/makeLogin.js
+++ src/utils/makeLogin.js
@@ -1,21 +1,33 @@
-import axios from "axios";
+import axios from "axios"
 
 export default async function makeLogin(email, password) {
   try {
-    const response = await axios.post("http://localhost:3000/api/login", {
-      email: email,
-      senha: password,
-    });
+    const response = await axios.post(
+      "http://localhost:3000/api/login",
+      {
+        email: email,
+        senha: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
 
-    console.log(response.data);
+    console.log(response.data)
 
     if (response.data.acesso) {
       const userCookie = response.data.cookie
       const userId = response.data.id
-      return {serverResponse: response.data.acesso, cookie: userCookie, id: userId}
+      return {
+        serverResponse: response.data.acesso,
+        cookie: userCookie,
+        id: userId,
+      }
     }
   } catch (error) {
-    console.log("Failed to make login:" + error);
-    throw error;
+    console.log("Failed to make login:" + error)
+    throw error
   }
 }
