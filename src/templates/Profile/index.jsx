21b0165101e185c08b6401f3ca2027cacce5513b
matheusfdosan <matheusfaustinoe20@gmail.com>
2024-11-21 11:28:06 -0300

    :construction: user profile in progress

--- src/templates/Profile/index.jsx
+++ src/templates/Profile/index.jsx
@@ -20,35 +20,18 @@ export default function Profile() {
       <Header />
       <main id="profile_container">
         <div id="user_info">
-          <div id="profile_picture_container">
-            <img
-              src="https://www.designi.com.br/images/preview/12161378.jpg"
-              alt="user_picture"
-            />
-          </div>
-
           <div id="user_datas">
             <h3>Gregory Singleton</h3>
-            <ul>
-              <li>
-                <span className="amout">13</span>
-                <span className="of_something">Seguidores</span>
-              </li>
-              <li>
-                <span className="amout">43</span>
-                <span className="of_something">Seguindo</span>
-              </li>
-              <li>
-                <span className="amout">3</span>
-                <span className="of_something">Postagens</span>
-              </li>
-            </ul>
+            <div>
+              <span className="amout">3</span>
+              <span className="of_something">Postagens</span>
+            </div>
           </div>
         </div>
 
         <h2>Suas Postagens</h2>
-          <Posts />
-        </main>
+        <Posts />
+      </main>
 
       <div id="profile_footer">
         <FooterLinks />
